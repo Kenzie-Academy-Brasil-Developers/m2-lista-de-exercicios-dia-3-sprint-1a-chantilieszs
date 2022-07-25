b29164e2372c828142fe8f8be9aae5c86ed64007
unknown <gelanlucas@gmail.com>
2022-07-25 19:46:06 -0300

    Entrega

--- src/script.js
+++ src/script.js
@@ -1,13 +1,78 @@
-function aboveAverageGrades() {}
+let grades = [9, 10, 8, 2, 1, 9, 0, 6, 7, 3, 8]
 
-function averageGrades() {}
 
-function belowAverageGrades() {}
+function aboveAverageGrades(notas) {
 
-function sumOfAllGrades() {}
+    let acimaDaMedia = []
 
-function averageOfGrades() {}
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] > 8){
+            acimaDaMedia.push(notas[i])
+        }
+    }
+    return acimaDaMedia
+}
+console.log(aboveAverageGrades(grades))
 
-function maxGrade() {}
+function averageGrades(notas) {
+    notasNaMedia = []
 
-function minGrade() {}
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] === 8){
+            notasNaMedia.push(notas[i])
+        }
+    }
+  
+    return notasNaMedia
+}
+console.log(averageGrades(grades));
+
+function belowAverageGrades(notas) {
+
+    notasAbaixoDaMedia = []
+
+    for (let i = 0; i < notas.length; i++) {
+        if (notas[i] < 8) {
+            notasAbaixoDaMedia.push(notas[i])
+        }
+    }
+    return notasAbaixoDaMedia
+}
+console.log(belowAverageGrades(grades));
+
+function sumOfAllGrades(notas) {
+
+    let somaTotal = 0
+   
+    for (let i = 0; i < notas.length; i++) {
+ 
+        somaTotal += notas[i]
+    }
+    return somaTotal
+}
+console.log(sumOfAllGrades(grades));
+
+function averageOfGrades(notas) {
+
+    let media = []
+
+    for (let i = 0; i< notas.length; i++) {
+       media = sumOfAllGrades(notas)/notas.length
+    }
+    return media
+}
+console.log(averageOfGrades(grades));
+
+function maxGrade(notas) {
+
+    let maximo = Math.max(...notas)
+    return maximo
+}
+console.log(maxGrade(grades));
+
+function minGrade(notas) {
+
+    let min = Math.min(...notas)
+    return min
+}
+console.log(minGrade(grades));
